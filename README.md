6dd0c11351de18c8c531f70a22fed7dc45d1d66c
0x63lv <8789226+0x63lv@users.noreply.github.com>
2024-01-13 14:49:41 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Open URLs in browser
+
+Adds a command that opens all the URLs in a selection in the default browser.
+
+## Functionality
+* Select a block of text
+* Choose "Open URLs in browser" from the command pallette
+* All URLs within the selected text block should open in the default browser window
+
+## Attributions
+[Backlink icons created by nawicon - Flaticon](https://www.flaticon.com/free-icons/backlink)
---
+++ extension.js
@@ -0,0 +1,54 @@
+const vscode = require('vscode');
+const opn = require('opn');
+const platform = require('os').platform();
+const defaultBrowser = require('x-default-browser');
+
+const WIN_CHROME = 'chrome';
+const OSX_CHROME = 'google chrome';
+const LINUX_CHROME = 'google-chrome';
+const LINUX_CHROMIUM = 'chromium-browser';
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+	let disposable = vscode.commands.registerCommand('open-urls-in-browser.open', function () {
+
+		const editor = vscode.window.activeTextEditor;
+		const selectedText = editor.document.getText(editor.selection)
+		const urlRegexp = /(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$])/igm;
+		const urls = [...selectedText.matchAll(urlRegexp)];
+
+		for (let i = 0; i < urls.length; i++) {
+			defaultBrowser(function (error, result) {
+				if (!error) {
+					let appName = null;
+					if (result.commonName === 'chrome') {
+						if (platform === 'win32') {
+							appName = WIN_CHROME;
+						} else if (platform === 'darwin') {
+							appName = OSX_CHROME;
+						} else {
+							appName = LINUX_CHROME;
+						}
+					} else if (result.commonName === 'chromium') {
+						appName = LINUX_CHROMIUM;
+					}
+					opn(urls[i][0], { app: appName });
+				}
+				else {
+					opn(urls[i][0]);
+				}
+			});
+		}
+  });
+	context.subscriptions.push(disposable);
+}
+
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
